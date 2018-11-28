a2d70452ee5d419ded86690e47dca4d0a23849ff
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2018-11-27 20:53:13 -0500

    containers

--- main.js
+++ main.js
@@ -9,15 +9,10 @@
 
 // })
 $(document).ready(function() {
-    // grab the initial top offset of the navigation 
+
        var stickyNavTop = $('.navbar').offset().top;
-       
-       // our function that decides weather the navigation bar should have "fixed" css position or not.
-       var stickyNav = function(){
-        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-             
-        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-        // otherwise change it back to relative
+              var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); 
         if (scrollTop > stickyNavTop) { 
             $('.navbar').addClass('sticky');
         } else {
@@ -26,8 +21,8 @@ $(document).ready(function() {
     };
 
     stickyNav();
-    // and run it again every time you scroll
     $(window).scroll(function() {
         stickyNav();
     });
 });
+
